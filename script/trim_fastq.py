3443142c4a3f3fc56c5971a1b4a801c76f280801
fanyucai1 <fanyucai1@126.com>
2023-03-31 13:45:20 +0800

    no

--- script/trim_fastq.py
+++ script/trim_fastq.py
@@ -1,6 +1,9 @@
 ##  S Martínez-Puchol,  Itarte M ,  Rusiol M , et al. Exploring the diversity of coronavirus in sewage during COVID-19 pandemic: Don't miss the forest for the trees[J]. Science of The Total Environment, 2021, 800:149562.
 ##  Fontenele R S, Kraberger S, Hadfield J, et al. High-throughput sequencing of SARS-CoV-2 in wastewater provides insights into circulating variants[J]. Water Research, 2021, 205: 117710.
 ##  Baaijens J A, Zulli A, Ott I M, et al. Lineage abundance estimation for SARS-CoV-2 in wastewater using transcriptome quantification techniques[J]. Genome biology, 2022, 23(1): 236.
+##  Email:yucai.fan@illumina.com
+##  2023.03.31 version:1.0
+
 import os
 import subprocess
 import argparse
@@ -48,24 +51,21 @@ print(cmd)
 subprocess.check_call(cmd,shell=True)
 
 # align reads with bowtie2 and sort bam with samtools
-cmd="bowtie2 --threads %s -x %s -1 %s_1.fastq.gz -2 %s_2.fastq.gz -S %s_aligned.sam && rm -rf %s_1.fastq.gz %s_2.fastq.gz"%(args.thread,args.index,out,out,out,out,out)
+cmd="bowtie2 --threads %s -x %s -1 %s_1.fastq.gz -2 %s_2.fastq.gz -S %s.aligned.sam | tee %s.bowtie2.stdout && rm -rf %s_1.fastq.gz %s_2.fastq.gz"%(args.thread,args.index,out,out,out,out,out,out)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
 
-cmd="samtools sort %s_aligned.sam -o %s_sorted.bam -@ %s && samtools index %s_sorted.bam && rm -rf %s_aligned.sam"%(out,out,args.thread,out,out)
+cmd="samtools sort %s.aligned.sam -o %s.sorted.bam -@ %s && samtools index %s.sorted.bam && rm -rf %s.aligned.sam"%(out,out,args.thread,out,out)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
 
 # trim primers with ivar (soft clipping)
 # https://andersen-lab.github.io/ivar/html/manualpage.html
 # -e    Include reads with no primers
-cmd="ivar trim -e -i %s_sorted.bam -b %s -p %s.soft.clipped | tee %s.ivar.stdout && rm -rf %s_sorted.bam"%(out,args.bed,out,out,out)
+cmd="ivar trim -e -i %s.sorted.bam -b %s -p %s.soft.clipped | tee %s.ivar.stdout && rm -rf %s.sorted.bam %s.sorted.bam.bai"%(out,args.bed,out,out,out,out)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
-# Generate a tsv file tabulating the number of reads vs trimmer primer name in the bed file
-cmd="cat %s.ivar.stdout | grep -A 10000 \"Primer Name\" | head -n -5 > %s.primer_hit_counts.tsv"%(out,out)
-print(cmd)
-subprocess.check_call(cmd,shell=True)
+
 
 ## remove soft-clipped primers
 #https://jvarkit.readthedocs.io/en/latest/Biostar84452/
@@ -78,14 +78,14 @@ subprocess.check_call(cmd,shell=True)
 
 # extract fastqs
 # https://www.htslib.org/doc/samtools-fasta.html
-cmd="samtools fastq -1 %s.all_reads.fq -2 %s.all_reads.fq -S %s.single.fq %s.trimmed.bam"%(out,out)
+cmd="/software/samtools-v1.17/bin/samtools fastq %s.trimmed.bam >%s.resorted.fastq"%(out,out)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
 
 # Generate Pile-Up and variantCalling
 # https://github.com/CFSAN-Biostatistics/C-WAP/blob/main/startWorkflow.nf
 # -m    Minimum read depth to call variants (Default: 10)
-cmd="samtools mpileup -A -aa -d 0 -Q 0 -o %s.pile.up --reference %s %s.soft.clipped.sort.bam"%(out,args.reference,out)
+cmd="/software/samtools-v1.17/bin/samtools mpileup -A -aa -d 0 -Q 0 -o %s.pile.up --reference %s %s.soft.clipped.sort.bam"%(out,args.reference,out)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
 cmd="cat %s.pile.up | ivar variants -p %s.rawVarCalls -g %s -r %s -m 10"%(out,out,args.gff,args.reference)
@@ -97,12 +97,13 @@ subprocess.check_call(cmd,shell=True)
 # https://github.com/CFSAN-Biostatistics/C-WAP/blob/main/startWorkflow.nf
 cmd="bcftools mpileup -Ou -f %s %s.trimmed.bam | bcftools call --ploidy 1 -mv -Oz -o %s.calls.vcf.gz"%(args.reference,out,out)
 cmd+=" && bcftools index %s.calls.vcf.gz"%(out)
-cmd+=" && cat %s | bcftools consensus %s.calls.vcf.gz > %s.consensus.fa"%(args.reference,out,out)
+cmd+=" && cat %s | bcftools consensus %s.calls.vcf.gz -p %s > %s.consensus.fa"%(args.reference,out,args.prefix,out)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
+subprocess.check_call('sed -i \'1s/.*/>%s/\' %s.consensus.fa'%(args.prefix,out),shell=True)
 
 # Calculation of the consensus sequence is used to determine the predominant lineage
-cmd="pangolin --alignment %s.consensus.fa --threads 2 --outdir %s"%(out,args.outdir)
+cmd="pangolin --alignment %s.consensus.fa --threads 2 --outdir %s --outfile %s.lineage_report.csv --alignment-file %s.alignment.fasta"%(out,args.outdir,args.prefix,args.prefix)
 print(cmd)
 subprocess.check_call(cmd,shell=True)
 
